673afbe90ac9034cf978c95484ac9d28e130fec3
nashwanhabboosh <nashwan.habboosh@icloud.com>
2022-02-11 19:30:47 -0500

    Create the makeGuess function

--- main.cpp
+++ main.cpp
@@ -2,10 +2,59 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <algorithm>
 #include <fstream>
-#include<iomanip>
+#include <iomanip>
+
+// guess is taken and stored as an array of pairs 
+// first is the letter second is letter value
+// 0-not in word
+// 1-wrong location
+// 2-correct location
+std::pair<char,int>* makeGuess () {
+
+    // holds the current 5 letter word guess
+    std::string guess;
+    // holds all of the letters and the data on its correctness
+    std::pair<char,int>* data = new std::pair<char,int>[5];
+    // holds the color of each letter
+    std::string color;
+
+    // promps user and stores guess
+    std::cout<<"Enter Guess\n";
+    std::cin>>guess;
+    if (guess.length()!=5) {
+        std::cerr<<"ERROR: invalid guess, length must be 5 not "<<guess.length()<<std::endl;
+        return nullptr;
+    }
+
+    // gets data on each letter
+    std::cout<<"What color was each letter?\n";
+    for (short i = 0;i<5;i++) {
+
+        // adds each letter to data
+        data[i].first=color[i];
+
+        // gets the color for each letter
+        std::cout<<guess[i]<<std::endl;
+        std::cin>>color;
+        transform(color.begin(), color.end(), color.begin(), ::tolower);
+        if (color=="green") {
+            data[i].second=2;
+        } else if (color == "yellow") {
+            data[i].second=1;
+        } else if (color == "black") {
+            data[i].second=0;
+        } else {
+            std::cout<<"Invalid color, please type \"green\", \"yellow\" or \"black\"\n";
+        }
+    }
+    return data;
+}
 
 int main () {
+
+    makeGuess();
 
     std::ifstream istr ("word_list.txt");
     std::string temp;
@@ -41,5 +90,4 @@ int main () {
         std::cout<<std::endl;
     }
 
-    std::cout<<"show isaac how pull requests work";
 }
