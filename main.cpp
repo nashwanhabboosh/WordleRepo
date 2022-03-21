b93578e16621a0671e4db5477a1676cf1bc62880
nashwanhabboosh <nashwan.habboosh@icloud.com>
2022-03-21 10:35:29 -0400

    Removed redundant files and comments

--- main.cpp
+++ main.cpp
@@ -331,41 +331,4 @@ int main () {
             std::cout<<bestGuessByLetter(occurences,*words)<<" seems to be the best guess."<<std::endl;
         }
     }
-
-    
-
-
-
-    
-    
-
-    
-
-    
-
-    
-
-    
-
-    /*// iterate through each letter of each word
-    for (short i = 0;i<5;i++) {
-            // check if the letter is in the map
-            if (characters.find(temp[i])==characters.end()) {
-                // create a vector for the letter if it is not in the map yet
-                std::vector<int> tempvec;
-                for (short j = 0;j<6;j++) {
-                    if (j==i||j==5) tempvec.push_back(1);
-                    else tempvec.push_back(0);
-                }
-                characters.insert(std::make_pair(temp[i],tempvec));
-            // if the letter is already in the map, increment the end value and positional values
-            } else {
-                characters[temp[i]][i]++;
-                characters[temp[i]][5]++;
-            }
-        }*/
-
-    // write the collected data to the output file
-    
-
 }
